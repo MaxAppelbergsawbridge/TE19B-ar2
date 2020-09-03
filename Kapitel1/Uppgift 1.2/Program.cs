b311079082aed529d8892e4fc24242cab65dde99
Max Appelberg Sawbridge <max.appelbergsawbri@learnet.se>
2020-09-03 11:06:35 +0200

    Övat på redigera,escape,kommentarer Prg 03/09/2020

---
+++ Kapitel1/Uppgift 1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Ett Program som skriver fler rad kod
+*/
+using System;
+
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detta är ett program gjort i C#.");
+            Console.WriteLine("Programmet skriver ut text på flera rader.");
+            Console.WriteLine("Detta är programmets sista utskrift.");
+            // smidigare sätt att göra
+            Console.WriteLine("Detta är ett program gjort i C#.\n" +
+            "Programmet skriver ut\t text på flera rader.\n" +
+            "Detta \"är programmets\\ sista utskrift.\n");
+        }
+    }
+}
