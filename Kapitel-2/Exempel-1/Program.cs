70d49a05c29bac7c62893ef91bed64f1ead4ca71
Max Appelberg Sawbridge <max.appelbergsawbri@learnet.se>
2020-09-09 14:58:07 +0200

    Övat på variabler, konkatenera, inmatning

---
+++ Kapitel-2/Exempel-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exempel_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarera en variabel för namnet 
+            string namn = "";
+            string hobby = "";
+            string meddelande = "";
+            //Fråga om namn
+            Console.Write("Vad heter du? ");
+            // läs in namnet
+            namn = Console.ReadLine();
+            //fråga om hobby
+            Console.WriteLine("Vad har du för hobby?");
+            //Läs in  hobby
+            hobby = Console.ReadLine();
+            //sätter in meddelande 
+            meddelande = ("Hej! " + namn + ",du gillar " + hobby + ".");
+            //skriver ut meddelandet
+            Console.WriteLine(meddelande);
+        }
+    }
+}
---
+++ Kapitel1/Konsolen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Konsolen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ändra bakgrunds färg på terminalen
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            //Ändra textfärg på bakgrunden 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
