652e87615410a0c93d379f7992a11bbde722d97f
Max Appelberg Sawbridge <max.appelbergsawbri@learnet.se>
2020-09-10 12:07:52 +0200

    Övat på variabler, double,int och parse.

---
+++ Kapitel-2/Exempel-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exempel_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarera några variabler
+            string namn = "";
+            int ålder = 0;
+            int ålderÅr2025 = 0;//camelcase
+            string meddelande = "";
+            double längd = 0;
+            //namn
+            Console.Write("Vad heter du?");
+            namn = Console.ReadLine();
+            //längd
+            Console.Write("Hur lång är du?");
+            längd = double.Parse(Console.ReadLine());
+            //ålder 2020+5= åder 2025
+            Console.Write("Hur gammal är du?");
+            ålder = int.Parse(Console.ReadLine());
+            ålderÅr2025 = ålder + 5;
+            meddelande = " Hej! " + namn + " du är " + längd + "lång" + ålderÅr2025 + " år gammal om 2025 ";
+            Console.Write(meddelande);
+        }
+    }
+}
---
+++ Kapitel-2/uppgift-2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double längd = 0;
+            double längdVärldsrekord = 2.45;
+            string meddelande = "";
+            double skillnaden = 0;
+            Console.WriteLine("Hur högt kan du hoppa?");
+            //läsa hur högt användaren kan hoppa
+            längd = double.Parse(Console.ReadLine());
+            //beräkna skillnaden
+            skillnaden = längdVärldsrekord - längd;
+            meddelande = "du är" + skillnaden + "ifrån världsrekorden";
+            Console.WriteLine(meddelande);
+        }
+    }
+}
