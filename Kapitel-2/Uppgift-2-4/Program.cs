b2fb0dbfb94011036c083ea43bd14a995cb4d5ef
Max Appelberg Sawbridge <max.appelbergsawbri@learnet.se>
2020-09-16 14:57:15 +0200

    Övat på variabler och inmatning

---
+++ Kapitel-2/Uppgift-2-4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Läs in första argumentet
+            Console.WriteLine("Välj 1 tal");
+            int a = int.Parse(args[0]);
+            Console.WriteLine("Välj 1 till tal");
+            int b = int.Parse(args[1]);
+            int c = a + b;
+            Console.WriteLine("summa är" + c);
+        }
+    }
+}
---
+++ Kapitel-2/Uppgift-2-5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket år är det?");
+            int år = int.Parse(Console.ReadLine());
+            int tidTill2100 = 2100 - år;
+            Console.WriteLine("Det är " + tidTill2100 + "år tills år2100");
+        }
+    }
+}
