495aca407562f6392ae24a2e3a64adfb70630ea4
Max Appelberg Sawbridge <max.appelbergsawbri@learnet.se>
2020-09-24 11:07:55 +0200

    Projekt WIP

---
+++ Projekt-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Projekt_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //läs in värdeplatser av namnen
+            Console.WriteLine("Vad heter du?");
+            String namn = Console.ReadLine();
+
+
+
+            Console.WriteLine("Välj mellan 1,2,3,4,5,6");
+            int svar1 = int.Parse(Console.ReadLine);
+
+        }
+    }
+}
