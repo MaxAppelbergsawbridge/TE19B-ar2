9b6509680eb99d5a03283a2d504fd62677d6e73e
Max Appelberg Sawbridge <max.appelbergsawbri@learnet.se>
2020-10-01 11:06:10 +0200

    övat på if,slumptal,loopar

---
+++ Kapitel3/Gissa-ett-tal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Gissa_ett_tal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //fråga 5 gånger
+            for (int i = 0; i < 5; i++)
+            {
+                //Slumpa fram ett tal
+                Random random = new Random();
+                int slumptal = random.Next(7);
+                //läs in gissning
+                Console.WriteLine("Gissa ett tal (1-6)?");
+                int gissning = int.Parse(Console.ReadLine());
+                //Kolla om gissningen är korrekt 
+                if (gissning == slumptal)
+                {
+                    Console.WriteLine("Rätt svar!");
+                    i = 5;
+                }
+                else
+                {
+                    Console.WriteLine("Fel svar!");
+                }
+            }
+        }
+    }
+}
