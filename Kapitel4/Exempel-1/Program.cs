a63846e8a66b4ee76f4da0eea950143436867c66
Max Appelberg Sawbridge <max.appelbergsawbri@learnet.se>
2020-10-07 14:58:52 +0200

    Övat på if och switch

---
+++ Kapitel4/Exempel-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exempel_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //slumpa fram ett tal
+            Random tärning = new Random();
+            int slumptal = tärning.Next(1, 7);
+
+
+            while (true)
+            {
+                //be användaren att gissa och läs in 
+                Console.WriteLine("Gissa ett tal (1-6)");
+                int gissning = int.Parse(Console.ReadLine());
+
+                // Om användaren får rätt 
+                if (gissning == slumptal)
+                {
+                    Console.WriteLine("Rätt gissat!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Fel gissat!Försök igen");
+                }
+            }
+            //slut
+            Console.WriteLine("Tack för idag");
+
+        }
+    }
+}
---
+++ Kapitel4/Exempel-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exempel_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val = "";
+            //presentera en meny som visas tills användare säger stop
+            while (val != "3")
+            {
+                Console.WriteLine("välj något av följande tal?");
+                Console.WriteLine("1.addera 2 tal");
+                Console.WriteLine("2.multiplicera 2 tal");
+                Console.WriteLine("3. avsluta");
+                val = Console.ReadLine();
+                switch (val)
+                {
+
+                    case "1":
+                        Console.WriteLine("välj 1 tal");
+                        int addera1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("välj 1 tal");
+                        int addera2 = int.Parse(Console.ReadLine());
+                        int slutprodukt = addera1 + addera2;
+                        Console.WriteLine(slutprodukt);
+                        break;
+                    case "2":
+                        Console.WriteLine("välj 1 tal");
+                        int multiplicera1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("välj 1 tal");
+                        int multiplicera2 = int.Parse(Console.ReadLine());
+                        int slutprodukt2 = multiplicera1 * multiplicera2;
+                        Console.WriteLine(slutprodukt2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
