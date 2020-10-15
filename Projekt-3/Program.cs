48a9630ff4f7e4d3d40e4bdfef21e3c1bc006488
Max Appelberg Sawbridge <max.appelbergsawbri@learnet.se>
2020-10-15 11:08:59 +0200

    frågesport,övningsprov

--- Projekt-3/Program.cs
+++ Projekt-3/Program.cs
@@ -11,36 +11,52 @@ namespace Projekt_3
             //namn
             Console.WriteLine("Vad heter du?");
             string namn = Console.ReadLine();
-            //fråge bank
-            string frågaett = "Vilket är a?";
-            //quizmachine
+            //frågar spelaren olika frågor beroende på vilken varv det är på
             for (int i = 0; i < 3; i++)
             {
                 if (i == 0)
                 {
+                    //frågan
                     string rättsvar = "3";
-                    Console.WriteLine("fråga1 Vart är c?");
+                    Console.WriteLine("fråga1 Vart är a?");
                     Console.WriteLine("1.c");
                     Console.WriteLine("2.b");
                     Console.WriteLine("3.a");
+                    //läser in svaret
                     string svar = Console.ReadLine();
-                    if (svar == rättsvar)
+                    if (int.TryParse(svar, out svarout))
+                    {
+                        Console.WriteLine("Vad bra");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nej jag bad om ett tal, försök igen");
+                    }
+                    if (svarout == rättsvar)
                     {
                         Console.WriteLine("bra jobbat");
                         antaletRätt++;
                     }
                 }
-
-
                 if (i == 1)
                 {
+                    //frågan
                     string rättsvar = "1";
                     Console.WriteLine("fråga2:vart är c?");
                     Console.WriteLine("1.c");
                     Console.WriteLine("2.b");
                     Console.WriteLine("3.a");
+                    //läser in svaret
                     string svar = Console.ReadLine();
-                    if (svar == rättsvar)
+                    if (int.TryParse(svar, out svarout))
+                    {
+                        Console.WriteLine("Vad bra");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nej jag bad om ett tal, försök igen");
+                    }
+                    if (svarout == rättsvar)
                     {
                         Console.WriteLine("bra jobbat");
                         antaletRätt++;
@@ -49,31 +65,52 @@ namespace Projekt_3
                 }
                 if (i == 2)
                 {
+                    //frågan
                     string rättsvar = "2";
                     Console.WriteLine("fråga2: Vart är b?");
                     Console.WriteLine("1.c");
                     Console.WriteLine("2.b");
                     Console.WriteLine("3.a");
+                    //Läser in svaret
                     string svar = Console.ReadLine();
-                    if (svar == rättsvar)
+                    if (int.TryParse(svar, out svarout))
+                    {
+                        Console.WriteLine("Vad bra");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nej jag bad om ett tal, försök igen");
+                    }
+                    if (svarout == rättsvar)
                     {
                         Console.WriteLine("bra jobbat");
                         antaletRätt++;
                     }
                 }
-
+                //Frågar om spelaren vill spela om
+                if (i == 2)
+                {
+                    Console.WriteLine("vill du spela om?");
+                    string slutsvar = Console.ReadLine();
+                    if (int.TryParse(slutsvar, out slutSvarout))
+                    {
+                        Console.WriteLine("Vad bra");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nej jag bad om ett tal, försök igen");
+                    }
+                    slutSvarout = slutSvarout.ToLower();
+                    if (slutSvarout == "ja")
+                    {
+                        i = -1;
+                        Console.WriteLine(antaletRätt);
+                    }
+                }
             }
-            Console.WriteLine("vill du spela om?");
-            string slutsvar = Console.ReadLine();
-            slutsvar = slutsvar.ToLower();
-            if (slutsvar == "ja")
-            {
-                int i = 0;
-                Console.WriteLine(antaletRätt);
-            }
-            Console.WriteLine(antaletRätt);
+            //När frågesporten är klar
+            Console.WriteLine(namn + "hade" + antaletRätt + "frågor rätt!");
             Console.WriteLine("Tack för att du spelade!");
-
         }
     }
 }
---
+++ Övnings-prov/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Övnings_prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //x*6=42
+            int rättsvar = 0;
+            int svar = 0;
+            Random numner = new Random();
+            int a = numner.Next(1, 9);
+            int b = numner.Next(1, 9);
+            Console.WriteLine("x*" + b + "=" + a * b);
+            Console.WriteLine("Vad ska x vara?");
+            while (a != svar)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out svar))
+                {
+                    if (svar == a)
+                    {
+                        Console.WriteLine("Bra jobbat");
+                        rättsvar++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("nej" + svar + "*" + b + "=" + svar * b);
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Skriv ett tal tack");
+                }
+            }
+            Console.WriteLine(rättsvar + "frågor rätt");
+            /*a1*a2
+            svar=a1 
+            a2=6 
+            */
+        }
+    }
+}
