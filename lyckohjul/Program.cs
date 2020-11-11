e47bce7b090c67e69be070c2ee46ff4025b91cd4
Max Appelberg Sawbridge <max.appelbergsawbri@learnet.se>
2020-11-11 14:58:39 +0100

    tärning och lyckhjul uppgift

---
+++ lyckohjul/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lyckohjul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // be användaren om ett tal
+            Console.WriteLine("Gissa vilken tal markören kommer stanna på");
+            string svar = Console.ReadLine();
+            int intsvar = 0;
+            while (!int.TryParse(svar, out intsvar))
+            {
+                Console.Write("Skriv in ett numner");
+                Console.ReadLine();
+            }
+            //slumpa fram ett tal
+            Random slumptal = new Random();
+            int lyckohjul = slumptal.Next(1, 11);
+            //berätta för användaren vad slumptalet blev
+            Console.Write($"talet var" + lyckohjul);
+            if (lyckohjul == intsvar)
+            {
+                Console.Write("Du hade rätt");
+            }
+            else
+            {
+                Console.Write("Du hade fel");
+            }
+        }
+    }
+}
---
+++ tärningsimulator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace tärningsimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hur många tärningar vill du kasta?");
+            int tärningar = 0;
+            string svar1 = Console.ReadLine();
+            while (!int.TryParse(svar1, out tärningar))
+            {
+                Console.WriteLine("Försök igen");
+                svar1 = Console.ReadLine();
+            }
+            Console.WriteLine("och hur många sidor ska alla ha?");
+            int sida = 0;
+            string svar2 = Console.ReadLine();
+            while (!int.TryParse(svar2, out sida))
+            {
+                Console.WriteLine("Försök igen");
+                svar1 = Console.ReadLine();
+            }
+            Random Slumptal = new Random();
+            for (int i = 0; i < tärningar; i++)
+            {
+                int tärning = Slumptal.Next(1, sida + 1);
+                Console.WriteLine(tärning);
+            }
+        }
+    }
+}
