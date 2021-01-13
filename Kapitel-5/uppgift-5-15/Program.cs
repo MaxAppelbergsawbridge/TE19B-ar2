b44312ab22b73618541b79b0c1cb7f0d3d686601
why u ask me now computer <mappelbridge@gmail.com>
2021-01-13 15:00:29 +0100

    spel

--- Kapitel-5/uppgift-5-15/Program.cs
+++ Kapitel-5/uppgift-5-15/Program.cs
@@ -5,15 +5,34 @@ namespace uppgift_5_16
     class Program
     {
         static void Main(string[] args)
+
         {// alfabet a=a i namn
             string alfabet = "abdefghijklmnopqrstuvwxyzåäö";
             string[] namn = { "Beth", "Liz", "Winston", "a", "b", "c" };
-            for (int i = 0; i < namn[i].Length; i++)
+            string example = " ";
+            string compare = " ";
+            int[] points = new int[5];
+            for (int i = 0; i < namn.Length; i++)
             {
-                string example = namn[i];
-
+                example = namn[i];
+                System.Console.WriteLine(example);
+                for (int x = 0; x < example.Length; x++)
+                {
+                    System.Console.WriteLine(x);
+                    for (int y = 0; y < alfabet.Length; y++)
+                    {
+                        compare += example;
+                        System.Console.WriteLine(example);
+                        for (int j = 0; j < points.Length; j++)
+                        {
+                            System.Console.WriteLine(points[j]);
+                        }
+                        points[i] = alfabet.IndexOf(compare);
+                    }
+                }
+                //whatever has the most points[i]=namn[i] 3,5,6,1,2 6,5,3,2,1,i=3/points[3]=1,
             }
+
         }
     }
-}
 }
---
+++ dataspelmapp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace dataspelmapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Skapa en matris med strängar
+            int[,] karta = {
+                {1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 1},
+                {1, 0, 1, 1, 1, 0, 1},
+                {1, 0, 1, 1, 0, 0, 1},
+                {1, 0, 1, 1, 0, 1, 1},
+                {1, 0, 0, 0, 0, 0, 1},
+                {1, 0, 1, 1, 1, 1, 1}
+            };
+            //var är spelare
+            int spelarePositiony = 6;
+            int spelarePositionx = 2;
+            Random slumpmotor = new Random();
+            int antalringar = 3;
+            while (antalringar != 0)
+            {
+                int x = slumpmotor.Next(0, 7);
+                int y = slumpmotor.Next(0, 7);
+                //om vi hamnar i en gång
+                if (karta[y, x] == 0)
+                {
+                    karta[y, x] = 2;
+                    antalringar--;
+                }
+
+            }
+            while (true)
+            {
+                Console.Clear();
+
+                karta[spelarePositiony, spelarePositionx] = 3;
+                for (int y = 0; y < 7; y++)
+                {
+                    for (int x = 0; x < 7; x++)
+                    {
+                        switch (karta[y, x])
+                        {
+                            case 1: // En vägg
+                                Console.Write('\u2B1C');
+                                break;
+                            case 2://poäng
+                                Console.Write("⭕");
+                                break;
+                            case 3:
+                                System.Console.Write("⭐");
+                                break;
+                            default: // En gång
+                                Console.Write('\u2B1B');
+                                break;
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                System.Console.WriteLine("W,A,S,D");
+                string direction = Console.ReadLine().ToUpper().Substring(0, 1);
+
+                switch (direction)
+                {
+                    case "A":
+                        spelarePositionx--;
+                        break;
+                    case "S":
+                        spelarePositiony++;
+                        break;
+                    case "D":
+                        spelarePositionx++;
+                        break;
+                    case "W":
+                        spelarePositiony--;
+                        break;
+                    case "C":
+                        break;
+                    default:
+                        System.Console.WriteLine("försök igen");
+                        break;
+                }
+                karta[spelarePositiony, spelarePositionx] = 3;
+            }
+        }
+    }
+}
